01753f64bac0a2c91eec48468b5daf7d7f732562
Asam <changhui1118@outlookl.com>
2018-05-12 17:38:14 +0800

    pagination模块

--- project/004-search-github/js/pagination/pagination.js
+++ project/004-search-github/js/pagination/pagination.js
@@ -25,26 +25,38 @@ function init(init_config){
     
     render_pagination_structure();
     calc_amount_page();
-    
-    render_pagination_list();
     change_current_page(config.current_page);
+    render_pagination_list();
+    // disable();
+}
+
+function enable(){
+    pagination_fieldset.disabled = true;
+}
+
+function disable(){
+    pagination_fieldset.disabled = true;
 }
 
 function render_pagination_structure(){
     pagination_wrap.classList.add('pagination');
     pagination_wrap.innerHTML = `
-    <div class="pagination-pre">
-        <button class="pagination-first">First</button>
-        <button class="pagination-prev">Prev</button>
-    </div>
-    <div class="pagination-list"></div>
-    <div class="pagination-post">
-        <button class="pagination-next">Next</button>
-        <button class="pagination-last">Last</button>
-    </div>
+    <fieldset class="pagination-fieldset">
+        <div class="pagination-pre">
+            <button class="pagination-first">First</button>
+            <button class="pagination-prev">Prev</button>
+        </div>
+        <div class="pagination-list"></div>
+        <div class="pagination-post">
+            <button class="pagination-next">Next</button>
+            <button class="pagination-last">Last</button>
+        </div>
+    </fieldset>
     `;
 
+    pagination_fieldset = pagination_wrap.getElementsByClassName('pagination-fieldset')[0];
     pagination_list = pagination_wrap.getElementsByClassName('pagination-list')[0];
+    
     pagination_wrap.addEventListener('click', function (e){
         let   first_btn = e.target.classList.contains('pagination-first')
             , prev_btn = e.target.classList.contains('pagination-prev')
@@ -70,9 +82,12 @@ function render_pagination_structure(){
 function render_pagination_list(){
     pagination_list.innerHTML = '';
 
-    let length_page = amount_page > config.range_page ? config.range_page : amount_page;
+    let between = calc_start_and_end_page()
+      , start_page = between.start_page
+      , end_page = between.end_page
+      ;
  
-    for(let i = 1; i <= length_page; i++){
+    for(let i = start_page; i <= end_page; i++){
         let btn = document.createElement('button');
         btn.innerHTML = i;
         btn.classList.add('pagination-item');
@@ -82,6 +97,33 @@ function render_pagination_list(){
         
         pagination_list.appendChild(btn);
     }
+}
+
+function calc_start_and_end_page(){
+    let start_page 
+      , middle = Math.ceil(config.range_page / 2)
+      , end_page
+  ;
+  
+  //可视页码在左边
+  if(config.current_page <= middle){ 
+    start_page = 1;
+    end_page = config.range_page;
+  }
+
+  //可视页码在右边
+  else if(config.current_page >= amount_page - middle){ 
+    start_page = amount_page - config.range_page + 1;
+    end_page = amount_page;
+  }
+
+  //可视页码在中间
+  else{   
+    start_page = config.current_page - middle + 1;
+    end_page = config.current_page + middle - 1;
+  }
+
+  return {start_page: start_page, end_page: end_page}
 }
 
 function change_current_page(current_page){
