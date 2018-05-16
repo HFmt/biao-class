47eae21e07298d30a19a2b2708f54d02473d17ef
Asam <changhui1118@outlookl.com>
2018-05-16 23:09:37 +0800

    数组增删改查

--- practice/039-todo/js/index.js
+++ practice/039-todo/js/index.js
@@ -1,8 +1,6 @@
-
 let find = require('./find_module');
 
-    let id_current = 1;
-    let todo_list = [
+let todo_list = [
     {
         id: id_current,
         name: '买菜',
@@ -10,51 +8,47 @@ let find = require('./find_module');
     },
     ];
 
+function Todo(id, list){
+    this.id = id;
+    this.list = list;
+}
+
+Todo.prototype.add = add;
+Todo.prototype.add = remove;
+Todo.prototype.add = modify;
+Todo.prototype.add = read;
+
+
 //增
     function add(row){
-        row.id = ++id_current;
-        todo_list.push(row);
+        row.id = ++this.id;
+        this.list.push(row);
     }
 
 //删
     function remove(id){
         if(id < 1)
             return;
-        todo_list.splice(find.find_index_by_id_for(todo_list, id), 1);
+        this.list.splice(find.find_index_by_id_for(this.list, id), 1);
     }
 
 //改
     function modify(id, new_row){
         if(id < 1)
             return;
-        let index = find.find_index_by_id_for(todo_list, id);
-        let old_row = todo_list[index];
-        todo_list[index] = Object.assign({}, old_row, new_row);
+        let index = find.find_index_by_id_for(this.list, id);
+        let old_row = this.list[index];
+        this.list[index] = Object.assign({}, old_row, new_row);
     }
 
 // 查
     function read(id){
         if(id)
-            console.log(find.find_index_by_id_for(todo_list, id)) ;
+            console.log(find.find_index_by_id_for(this.list, id)) ;
         else
-            console.log(todo_list) ;
+            console.log(this.list) ;
     }
 
-    add({
-        name: '切菜',
-        completed: false,
-    });
-    // console.log(todo_list);
-  
+let todo = new Todo();
 
-    // remove(1);
-    // console.log(todo_list);
-
-    read(1);
-
-    modify(1, {
-        name: '洗菜'
-    });
-    // console.log(todo_list);
-
-    console.log(find.find_row_by_id_for(todo_list, 1)) ;
+console.log(todo);
